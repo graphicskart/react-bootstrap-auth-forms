f353ff8c7f9142bf269076d21a9bdda71652ba6c
graphicskart <graphicskart2018@gmail.com>
2019-02-23 10:58:18 +0530

    Updates

---
+++ precache-manifest.655e622103f700e3f30d410fbd5d2353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27366d025eae06038cca",
+    "url": "/react-bootstrap-auth-forms/static/js/runtime~main.27366d02.js"
+  },
+  {
+    "revision": "d45ebe0885498adfa1f9",
+    "url": "/react-bootstrap-auth-forms/static/js/main.d45ebe08.chunk.js"
+  },
+  {
+    "revision": "f9a4cd0dc292dc68744f",
+    "url": "/react-bootstrap-auth-forms/static/js/2.f9a4cd0d.chunk.js"
+  },
+  {
+    "revision": "d45ebe0885498adfa1f9",
+    "url": "/react-bootstrap-auth-forms/static/css/main.ad43a208.chunk.css"
+  },
+  {
+    "revision": "1d3b0ce952a020a7a58b5056ed77e116",
+    "url": "/react-bootstrap-auth-forms/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-bootstrap-auth-forms/precache-manifest.655e622103f700e3f30d410fbd5d2353.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-bootstrap-auth-forms/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
